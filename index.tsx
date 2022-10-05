e1e6a42ec2307809bbe4e872c5bb5d7a489e763f
maxivy138 <maxivymax@gmail.com>
2022-10-04 22:04:53 -0700

    Updates

---
+++ index.tsx
@@ -0,0 +1,43 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
+import { FeaturedPosts } from '../sections';
+import { Key } from 'react';
+
+export default function Home({ posts, index } : { posts: any, index: any }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Surpleg | Surpleg's Dump</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+              {posts.map((post: { node: any; }, index: Key | null | undefined) => (
+                <PostCard key={index} post={post.node} />
+                ))}
+        </div>
+
+      <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget categories={undefined} slug={undefined} />
+            <Categories />
+          </div>
+      </div>
+      </div>
+     
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
+
